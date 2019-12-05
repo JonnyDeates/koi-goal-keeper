191a5fc62516023747726e96970abbc349f43f29
Jdeates <jonnydeates@gmail.com>
2019-12-05 18:34:55 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://git.heroku.com/fathomless-caverns-68014.git',
+    API_ENDPOINT: 'https://fathomless-caverns-68014.herokuapp.com',
     TOKEN_KEY: 'blogful-client-auth-token',
 }
