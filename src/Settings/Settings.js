354d1789ded57443218c14c2bca5fd452c7c97bb
Jonny Deates <jonnydeates@gmail.com>
2020-08-05 14:52:51 -0700

    Mobile Version

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -98,23 +98,25 @@ class Settings extends React.Component {
 
     toggleLocal() {
         GoalService.saveGoals(this.props.goalListContext.currentGoals);
-        this.context.toggleLocalStorage(()=> {
+        this.context.toggleLocalStorage(() => {
             GoalApiService.purgeGoals();
-            setTimeout(()=> this.props.goalListContext.currentGoals.forEach((goal) => {
-                    GoalApiService.postGoal(goal).then((res) => {
-                        goal.goals.forEach(obj => {
-                            obj.goalid = res.id;
-                            ObjectivesApiService.postObjective(obj)
-                        })
-                    })}),100);
+            setTimeout(() => this.props.goalListContext.currentGoals.forEach((goal) => {
+                GoalApiService.postGoal(goal).then((res) => {
+                    goal.goals.forEach(obj => {
+                        obj.goalid = res.id;
+                        ObjectivesApiService.postObjective(obj)
+                    })
+                })
+            }), 100);
             PastGoalApiService.purgePastGoals();
-            setTimeout(()=> this.props.goalListContext.pastGoals.forEach((goal) => {
+            setTimeout(() => this.props.goalListContext.pastGoals.forEach((goal) => {
                 PastGoalApiService.postPastGoal(goal).then((res) => {
                     goal.goals.forEach(obj => {
                         obj.goalid = res.id;
                         PastObjectivesApiService.postObjective(obj)
                     })
-                })}),100);
+                })
+            }), 100);
         });
 
     }
@@ -130,7 +132,8 @@ class Settings extends React.Component {
                 <h1 style={{color: getCurrentThemeColors().headerColor}}>Profile</h1>
                 <div className='bar-indicator-top' style={getColor(this.context.currentType)}/>
                 <section>
-                    <h2 title={'UserId'} style={{color: getCurrentThemeColors().headerColor}}>{this.context.username}</h2>
+                    <h2 title={'UserId'}
+                        style={{color: getCurrentThemeColors().headerColor}}>{this.context.username}</h2>
                     {(this.state.isUsernameEditable)
                         ? <form onSubmit={(e) => this.submitNickname(e)}>
                             <label className='even-space' style={{color: getCurrentThemeColors().fontColor}}
@@ -162,24 +165,26 @@ class Settings extends React.Component {
                 </section>
                 <section>
                     {(this.state.isPasswordEditable) ? <form onSubmit={(e) => this.submitPassword(e)}>
-                        <label className='even-space' title={'Old Password'} style={{color: getCurrentThemeColors().fontColor}}>Old Password<input type='password'
-                                                                                                title={'Old Password Input'}
-                                                                                                onChange={(e) => this.changeOldPasswordCheck(e)}
-                                                                                                value={this.state.oldPasswordCheck}
-                                                                                                onKeyPress={e => {
-                                                                                                    if (e.key === 'Enter') {
-                                                                                                        e.preventDefault();
-                                                                                                    }
-                                                                                                }}/></label>
-                        <label className='even-space' title={'New Password'} style={{color: getCurrentThemeColors().fontColor}}>New Password<input type='password'
-                                                                                                title={'New Password Input'}
-                                                                                                onChange={(e) => this.changePassword(e)}
-                                                                                                value={this.state.newPassword}
-                                                                                                onKeyPress={e => {
-                                                                                                    if (e.key === 'Enter') {
-                                                                                                        e.preventDefault();
-                                                                                                    }
-                                                                                                }}/></label>
+                        <label className='even-space' title={'Old Password'}
+                               style={{color: getCurrentThemeColors().fontColor}}>Old Password<input type='password'
+                                                                                                     title={'Old Password Input'}
+                                                                                                     onChange={(e) => this.changeOldPasswordCheck(e)}
+                                                                                                     value={this.state.oldPasswordCheck}
+                                                                                                     onKeyPress={e => {
+                                                                                                         if (e.key === 'Enter') {
+                                                                                                             e.preventDefault();
+                                                                                                         }
+                                                                                                     }}/></label>
+                        <label className='even-space' title={'New Password'}
+                               style={{color: getCurrentThemeColors().fontColor}}>New Password<input type='password'
+                                                                                                     title={'New Password Input'}
+                                                                                                     onChange={(e) => this.changePassword(e)}
+                                                                                                     value={this.state.newPassword}
+                                                                                                     onKeyPress={e => {
+                                                                                                         if (e.key === 'Enter') {
+                                                                                                             e.preventDefault();
+                                                                                                         }
+                                                                                                     }}/></label>
                         <label className='even-space' title={'Confirm New Password'}
                                style={{color: getCurrentThemeColors().fontColor}}>Confirm Password<input
                             type='password' title={'Confirm New Password Input'}
@@ -207,67 +212,91 @@ class Settings extends React.Component {
                     <section className={'theme-list'}>
                         <div className={'dropdown-wrapper'}>
                             <div className='dropdown-types dropdown-types-settings'
-                                 style={{backgroundColor: getCurrentThemeColors().tColor, color: getCurrentThemeColors().fontColor}}>
+                                 style={{
+                                     backgroundColor: getCurrentThemeColors().tColor,
+                                     color: getCurrentThemeColors().fontColor
+                                 }}>
                                 <span>{this.context.theme}</span>
                                 <ul className={'dropdown-list'}>
                                     {this.context.themes.map((theme, i) => <li key={i}
                                                                                className={(this.context.currentTheme === theme) ? 'tinted' : ''}
-                                                                               style={{backgroundColor:  getThemes().find((t)=> t.name === theme).tColor,
-                                                                                   color: getThemes().find((t)=> t.name === theme).fontColor}}
+                                                                               style={{
+                                                                                   backgroundColor: getThemes().find((t) => t.name === theme).tColor,
+                                                                                   color: getThemes().find((t) => t.name === theme).fontColor
+                                                                               }}
                                                                                onClick={() => this.setTheme(theme)}>{theme}</li>)}
                                 </ul>
                             </div>
                         </div>
                     </section>
-                    </div>
+                </div>
                 <section>
                     <p className='even-space' onClick={() => this.context.toggleArchiving()}
-                           title={'Click To Toggle The Automatic Daily Archiver'}
-                           style={{color: getCurrentThemeColors().fontColor}}>
+                       title={'Click To Toggle The Automatic Daily Archiver'}
+                       style={{color: getCurrentThemeColors().fontColor}}>
                         Automatic Daily Archiver
                         <span
-                            style={{color: getCurrentThemeColors().headerColor}}>{(this.context.autoArchiving) ? 'On' : 'Off'}</span></p>
+                            style={{color: getCurrentThemeColors().headerColor}}>{(this.context.autoArchiving) ? 'On' : 'Off'}</span>
+                    </p>
                 </section>
                 <section>
-                    <p className='even-space' onClick={()=> this.setState({localStorageModel: true})}
-                           title={'Click To Toggle The Local Storage'}
-                           style={{color: getCurrentThemeColors().fontColor}}>
+                    <p className='even-space' onClick={() => this.setState({localStorageModel: true})}
+                       title={'Click To Toggle The Local Storage'}
+                       style={{color: getCurrentThemeColors().fontColor}}>
                         Local Storage
                         <span
-                            style={{color: getCurrentThemeColors().headerColor}}>{(this.context.localStorage) ? 'On' : 'Off'}</span></p>
+                            style={{color: getCurrentThemeColors().headerColor}}>{(this.context.localStorage) ? 'On' : 'Off'}</span>
+                    </p>
                 </section>
                 <section>
-                    <p className='even-space' onClick={() => {this.context.toggleDarkMode(); document.body.style.backgroundColor = getCurrentThemeColors().pColor;}}
+                    <p className='even-space' onClick={() => {
+                        this.context.toggleDarkMode();
+                        document.body.style.backgroundColor = getCurrentThemeColors().pColor;
+                    }}
                        title={'Click To Toggle Dark Mode'}
                        style={{color: getCurrentThemeColors().fontColor}}>
                         Dark Mode
                         <span
-                            style={{color: getCurrentThemeColors().headerColor}}>{(this.context.darkMode) ? 'On' : 'Off'}</span></p>
+                            style={{color: getCurrentThemeColors().headerColor}}>{(this.context.darkMode) ? 'On' : 'Off'}</span>
+                    </p>
                 </section>
                 <section>
                     <p className='even-space noselect' onClick={() => this.context.toggleType()}
-                       title={'Click To Toggle Through The Types List'} style={{color: getCurrentThemeColors().fontColor}}>
+                       title={'Click To Toggle Through The Types List'}
+                       style={{color: getCurrentThemeColors().fontColor}}>
                         Types List
                         <span
-                            style={{color: getCurrentThemeColors().headerColor}}>{this.context.typeListSelected}</span></p>
+                            style={{color: getCurrentThemeColors().headerColor}}>{this.context.typeListSelected}</span>
+                    </p>
                 </section>
                 <section className={'color-style'}>
-                    <li>{findTypeColor().colors.map((color, i)=> <div key={i} style={{backgroundColor: (getTypeColorsAvailable().includes(i)) ? color : color+'11'}} title={getSpecificType(i)}>
-                        <span className={(getTypeColorsAvailable().includes(i)) ? '' : 'hidden'}>{getSpecificType(i)}</span>
-                    </div>)} </li>
                     <div>
-                        {getTypeColors().map((theme, i)=> <li key={i} onClick={() =>
-                            this.context.updateColorStyle(theme.type)}>
-                            {theme.colors.map((color, j)=>
-                                <div key={i+''+j} style={{backgroundColor: (getTypeColorsAvailable().includes(j)) ? color : color+'11'}}/>)}</li>)}
+                        <li>{findTypeColor().colors.map((color, i) => <div key={i}
+                                                                           style={{backgroundColor: (getTypeColorsAvailable().includes(i)) ? color : color + '11'}}
+                                                                           title={getSpecificType(i)}>
+                            <span
+                                className={(getTypeColorsAvailable().includes(i)) ? '' : 'hidden'}>{getSpecificType(i)}</span>
+                        </div>)} </li>
+                        <div>
+                            {getTypeColors().map((theme, i) => <li key={i} onClick={() =>
+                                this.context.updateColorStyle(theme.type)}>
+                                {theme.colors.map((color, j) =>
+                                    <div key={i + '' + j}
+                                         style={{backgroundColor: (getTypeColorsAvailable().includes(j)) ? color : color + '11'}}/>)}</li>)}
+                        </div>
                     </div>
                 </section>
                 <button className='delete' onClick={() => this.setState({deleteModel: true})}
-                        style={{backgroundColor: getCurrentThemeColors().tColor, color: getCurrentThemeColors().fontColor+'77'}}>Suspend
+                        style={{
+                            backgroundColor: getCurrentThemeColors().tColor,
+                            color: getCurrentThemeColors().fontColor + '77'
+                        }}>Suspend
                     Account
                 </button>
                 {(this.state.deleteModel) ? <DeleteModel closeModel={() => this.setState({deleteModel: false})}/> : ''}
-                {(this.state.localStorageModel) ? <LocalStorageModel closeModel={() => this.setState({localStorageModel: false})} toggleLocal={this.toggleLocal}/> : ''}
+                {(this.state.localStorageModel) ?
+                    <LocalStorageModel closeModel={() => this.setState({localStorageModel: false})}
+                                       toggleLocal={this.toggleLocal}/> : ''}
             </main>
         )
     }
