f4066b7985db6806f548c8dae77b2df249624aa4
Jdeates <jonnydeates@gmail.com>
2020-10-17 18:18:13 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000',//'https://koigoalkeeperapi.herokuapp.com', //
+    API_ENDPOINT: 'https://koigoalkeeperapi.herokuapp.com', //'http://localhost:8000',//
     TOKEN_KEY: 'kingkoinetworkdeates',
     GOOGLE_TOKEN: '210398171394-4tvu2p5580kl3d959vidn4avuif5p53n.apps.googleusercontent.com',
     STRIPE_PUBLIC: 'pk_test_51HKB0JJUCsOYSAkZdUq0wWjOTjTIzILbQjAy8Cgz0loq4KStnLZtzvQpFlo7J5EeDaxpf6ADLabZv0club1dafwG00y2Jh1rUZ', //'pk_live_51HKB0JJUCsOYSAkZRdHn90UezYoYsZdVBYf0zhf2MeNM1Z4qfEoISBRZECk0sArGbU0Lrq5mzDLzsSTdLGzqeL6x00lxYBqgjU'
